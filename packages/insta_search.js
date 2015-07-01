a73e6990912a64ea032f4d967d1e580fe7432425
prozeal <tangmy90327@gmail.com>
2015-07-01 02:42:02 -0700

    Initial Commit

---
+++ packages/insta_search.js
@@ -0,0 +1,29 @@
+/**
+ * Created by minyitang on 7/1/15.
+ */
+
+
+
+module.exports = {
+    /**
+     *
+     */
+    authInsta: function(){
+
+    },
+    /** Perfome a search functionality on Instagram, This method may only be called when the authentication is completed
+     *
+     * @param tag
+     */
+    searchInsta: function(tag){
+
+    },
+
+    /** Store the infromation returned by the search functionality on Instagram
+     *
+     * @param result
+     */
+    storeInstInfo: function(result){
+
+    }
+}
